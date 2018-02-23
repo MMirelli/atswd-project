36af33235e882e4ae4669d8f84561dbab9ff1660
MMirelli <massimilianomirelli.mm@gmail.com>
2018-02-23 08:39:03 +0100

    First commit

---
+++ com.project.examsystem/src/main/java/it/fi/mirelli/com/project/examsystem/App.java
@@ -0,0 +1,13 @@
+package it.fi.mirelli.com.project.examsystem;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
