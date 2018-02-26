929db19fcb1733dced8100291218d849de41b991
MMirelli <massimilianomirelli.mm@gmail.com>
2018-02-26 18:59:58 +0100

    travis_b sudo removed

--- travis_b.sh
+++ travis_b.sh
@@ -2,4 +2,4 @@
 
 mvn -f com.project.examsystem/pom.xml clean verify coveralls:report 
 mvn -f com.project.examsystem/pom.xml clean compile sonar:sonar 
-sudo mvn -f com.project.examsystem/pom.xml clean verify -P auto-docker-start-up
+mvn -f com.project.examsystem/pom.xml clean verify -P auto-docker-start-up
