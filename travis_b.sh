8a4a0456895ae2fdc4d6449638fc437b2eb9723d
MMirelli <massimilianomirelli.mm@gmail.com>
2018-02-28 22:49:35 +0100

    travis_b coveralls and sonar added again

--- travis_b.sh
+++ travis_b.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+mvn -f com.project.examsystem/pom.xml clean verify coveralls:report 
+mvn -f com.project.examsystem/pom.xml clean compile sonar:sonar
 mvn -f com.project.examsystem/pom.xml clean verify -P auto-docker-start-up
