22c4df66e3f09be8b209588b4d3a27bdcd640d06
MMirelli <massimilianomirelli.mm@gmail.com>
2018-03-04 00:18:31 +0100

    SonarCoCo .travis added a command

--- travis_b.sh
+++ travis_b.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 mvn -f student-project/pom.xml clean verify coveralls:report 
-mvn -f student-project/pom.xml clean compile sonar:sonar
+mvn -f student-project/pom.xml clean org.jacoco:jacoco-maven-plugin:prepare-agent package sonar:sonar
 mvn -f student-project/pom.xml clean verify -P IT-auto-docker-start-up
